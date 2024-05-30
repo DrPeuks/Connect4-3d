c7769606976440ed8c6000c76972be026eaaf1c2
Mathis Guihot <89562745+Zboubix@users.noreply.github.com>
2024-05-30 11:30:34 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+setup(
+    name='connect4-3d',
+    options={
+        'build_apps': {
+            # Build connect4-3d as a console application
+            'console_apps': {
+                'connect4-3d': 'main.py',
+            },
+
+            'icons': {
+                'connect4-3d': ["icons/icon-256.png", "icons/icon-128.png", "icons/icon-48.png", "icons/icon-32.png", "icons/icon-16.png"]
+            },
+
+
+            # Specify which files are included with the distribution
+            'include_patterns': [
+                'images/*.png',
+                'models/*.jpg',
+                'models/*.bam',
+
+            ],
+
+            'exclude_patterns': [
+                'repartition.txt'
+            ],
+
+            'platforms': ['win_amd64'], # remove this if you want to build for other platforms
+
+            # Include the OpenGL renderer
+            'plugins': [
+                'pandagl',
+            ],
+        }
+    }
+)
